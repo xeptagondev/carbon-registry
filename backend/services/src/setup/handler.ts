488e50b49ebe01abd1218c60fd428d2adde5cd71
gayanath8 <gayanathr@xeptagon.com>
2023-12-14 15:17:11 +0530

    Ministry Department Change

--- backend/services/src/setup/handler.ts
+++ backend/services/src/setup/handler.ts
@@ -122,6 +122,8 @@ export const handler: Handler = async (event) => {
           email: fields[1],
           phoneNo: fields[2],
           website: undefined,
+          ministry:undefined,
+          govDep:undefined,
           address: configService.get("systemCountryName"),
           logo: undefined,
           country: configService.get("systemCountry"),
